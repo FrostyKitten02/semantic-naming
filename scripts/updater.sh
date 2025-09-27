7a21f2d0e73b9d3e85e7f4d84313d4f7ad5992ea
FrostyKitten <alen.fridau02@gmail.com>
2025-09-27 23:06:51 +0200

    upaths fix

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-utils_file=./updater_utils.sh
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+utils_file="$SCRIPT_DIR/updater_utils.sh"
 
 # shellcheck source=./updater_utils.sh
 source "$utils_file"
 
 # shellcheck source=./args_parser.sh
-source ./args_parser.sh "$@" || exit 1
+source "$SCRIPT_DIR/args_parser.sh" "$@" || exit 1
 
 check_version_mode "$increase_version_mode" || exit 1
 increase_version "$current_version" "$increase_version_mode" "$remove_suffix" "$new_suffix" || exit 1
